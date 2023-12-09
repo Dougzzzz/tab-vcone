0c28b02f3d8d3201b541f22d586b68cffb58db38
Douglas Barcelos <94402488+Dougzzzz@users.noreply.github.com>
2023-12-09 19:13:31 +0000

    alterei a mensagema e a imagem

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import React from 'react';
 const App = () => {
     return (
       <body>
-        <h1>Feitiço</h1>
-                  
-          <img
-            src='https://i.gifer.com/74JU.gif'
+        <h1>Feitiço x2</h1>
+        <h2>Será que a piada foi longe demais?</h2>   
+        <img
+            src='https://media.tenor.com/Vk3eHZgEG_YAAAAM/wizard-magic.gif'
           />
         
       </body>
