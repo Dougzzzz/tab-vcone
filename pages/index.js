9ebe022edd5c9214a15ade33e2949f32b9626064
Douglas Barcelos <94402488+Dougzzzz@users.noreply.github.com>
2023-12-09 19:13:31 +0000

    mensagem atualizada com imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home() {
-    return <h1>Feitço!!</h1>
-}
+import { Html } from 'next/document';
+import React from 'react';
 
-export default Home
+const App = () => {
+    return (
+      <body>
+        <h1>Feitiço</h1>
+                  
+          <img
+            src='https://i.gifer.com/74JU.gif'
+          />
+        
+      </body>
+     
+    );
+  };
+  
+  export default App;
