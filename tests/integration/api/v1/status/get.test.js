758cd31b727bbbfe0085c8a5db48c7c4852584ef
Douglas Barcelos <94402488+Dougzzzz@users.noreply.github.com>
2025-01-12 19:10:39 +0000

    Incluído tratamento de erros no banco de dados

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("GET /api/v1/status/ should return 200", async () => {
   const resposta = await fetch("http://localhost:3000/api/v1/status");
   expect(resposta.status).toBe(200);
+
+  const responseBody = await resposta.json();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
