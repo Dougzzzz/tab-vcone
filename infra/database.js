36937573e5c141eb982b2670e5d875ae44d163e6
Douglas Barcelos <94402488+Dougzzzz@users.noreply.github.com>
2025-01-22 23:42:27 +0000

    banco de produção aiven adicionado configurado

--- infra/database.js
+++ infra/database.js
@@ -1,12 +1,16 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl:{
+      rejectUnauthorized: false
+    }
   });
 
   try {
