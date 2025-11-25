f7b4d4509ab6c02ec1a8c45b521ad0f83a7187ae
Douglas Barcelos <94402488+Dougzzzz@users.noreply.github.com>
2025-11-25 18:04:44 +0000

    orcheatrator and script test updated

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,11 +5,14 @@ async function waitForAllServices() {
     async function waitForWebServer() {
         await retry(fetchStatusPage,{
             retries: 100,
+            minTimeout: 1000,
         })
 
         async function fetchStatusPage() {
             const response = await fetch("http://localhost:3000/api/v1/status");
-            const responseBody = await response.json();
+            if (response.status !== 200) {
+                throw new Error(`Status page returned ${response.status}`);
+            }
         };
 
     };
